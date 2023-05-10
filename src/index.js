483d739ac56f5f3b6e69e0fa0f638ad176959ca0
DashaVoitovych <dashavoitovych@gmail.com>
2023-05-10 12:17:33 +0200

    apis

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ function formatDate(date) {
   let currentHour = date.getHours();
   let currentMinute = date.getMinutes();
   let currentYear = date.getFullYear();
-  let currentMonth = ("0" + (date.getMonth() + 1)).slice(-2);
-  let currentDate = `0${date.getDate()}`.slice(-2);
+  let currentMonth = date.getMonth();
+  let currentDate = date.getDate();
   let dayHour = document.querySelector("#day-hour");
   dayHour.innerHTML = `${currentDay}  ${currentHour}:${currentMinute}`;
   let fullTime = document.querySelector("#date");
@@ -24,6 +24,8 @@ function formatDate(date) {
 formatDate(currentTime);
 
 function showTemperature(response) {
+  console.log(response);
+  console.log(response.data.main.temp);
   let temperature = Math.round(response.data.main.temp);
   let city = response.data.name;
   let h1 = document.querySelector("h1");
@@ -35,6 +37,7 @@ function showTemperature(response) {
 function searchForCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-text-input");
+  console.log(cityInput.value);
 
   let apiKey = "017d56650cd168d68067850318775d43";
   let units = "metric";
@@ -46,19 +49,22 @@ let citySearch = document.querySelector("#city-search");
 citySearch.addEventListener("submit", searchForCity);
 
 function showTemp(response) {
-  let yourTemp = Math.round(response.data.main.temp);
+  console.log(response.data.temperature.current);
+  let yourTemp = Math.round(response.data.temperature.current);
   let currentTemp = document.querySelector("#current-temp");
   currentTemp.innerHTML = `${yourTemp}°`;
-  let currentCity = response.data.name;
+  let currentCity = response.data.city;
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${currentCity}`;
 }
 
 function showPosition(position) {
   let lat = position.coords.latitude;
+  console.log(lat);
   let lon = position.coords.longitude;
-  let apiKey = "597c40c39084687093b091cd48b366f8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  console.log(lon);
+  let apiKey = "84fcftd03of3d3844ecba1bafb7d3009";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemp);
 }
 
